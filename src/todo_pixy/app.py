84155da9aa6db368b27a1c8616f10fc866ea429f
Pradeep Roark <pradeep.roark@gmail.com>
2024-05-21 09:17:01 +0100

    Implement adding a todo

--- src/todo_pixy/app.py
+++ src/todo_pixy/app.py
@@ -1,8 +1,18 @@
-from http.client import OK
-from flask import Flask
+from flask import Flask, request
 from todo_pixy.templates import index
+from todo_pixy.templates.add_todo import (
+    add_todo_success_repsonse,
+)
 
 app = Flask(__name__, static_url_path="", static_folder="static")
+
+
+@app.route("/addTODO", methods=["POST"])
+def add_todo():
+    input_new_todo = request.form.get("new-todo")
+    if input_new_todo and input_new_todo.strip() != "":
+        return add_todo_success_repsonse(input_new_todo.strip())
+    return "Not a valid todo", 400
 
 
 @app.route("/")
